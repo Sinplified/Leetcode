72492d92451355d29aa7441ae815768af39e6866
Sindhu <70776993+Sinplified@users.noreply.github.com>
2022-02-03 06:38:43 +0530

    Time: 55 ms (45.27%), Space: 14.2 MB (96.17%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.py
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.py
@@ -1,50 +1,26 @@
 class Solution:
     
-    def bsearch(self,nums,start,end,target):
+    def search(self, nums: List[int], target: int) -> int:
+        
+        start,end = 0,len(nums)-1
+        
         while start<=end:
             mid = (start+end)//2
             
             if nums[mid] == target:
                 return mid
             
-            elif nums[mid]<target:
-                start = mid+1
-            
-            else:
-                end = mid-1
+            elif nums[mid]>= nums[start]:
+                if target >=nums[start] and target<nums[mid]:
+                    end = mid-1
                 
-        return -1
-    
-    def search(self, nums: List[int], target: int) -> int:
-        l = 0
-        r = len(nums)-1        
-        rotation = -1
-        
-        while l<=r:
-            mid = (l+r)//2
-            
-            if mid+1 > len(nums)-1:
-                break
-            
-            if nums[mid]> nums[mid+1]:
-                rotation = mid+1
-                break
-            
-            else:
-                
-                if nums[mid]<nums[0]:
-                    r = mid-1
-                    
                 else:
-                    l = mid+1
-        
-        
-        if rotation == -1:
-            return self.bsearch(nums,0,len(nums)-1,target)
-        
-        elif target<nums[0]:
-            return self.bsearch(nums,rotation,len(nums)-1,target)
-        
-        else:
-            return self.bsearch(nums,0,rotation-1,target)
-        
+                    start  = mid+1
+            
+            else:
+                if target <= nums[end] and target>nums[mid]:
+                    start = mid + 1
+                
+                else:
+                    end = mid-1
+        return -1

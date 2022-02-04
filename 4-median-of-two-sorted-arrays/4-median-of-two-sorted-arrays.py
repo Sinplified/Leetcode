c662718914c8dc4c420226ae029028ff41852e7b
Sindhu <70776993+Sinplified@users.noreply.github.com>
2022-02-04 08:04:16 +0530

    Time: 96 ms (72.10%), Space: 14.1 MB (96.75%) - LeetHub

--- 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.py
+++ 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.py
@@ -1,67 +1,35 @@
 class Solution:
-    '''returns the count of numbers less than or equal to mid'''
-    def ncalc(self,nums,med):
+    
+    def median(self,nums1,nums2,m,n):
         
-        start = 0
-        end = len(nums)-1
+        if m>n:
+           return self.median(nums2,nums1,n,m) 
         
-        while start <= end:
-            mid = (start + end)//2
+        low , high = 0 , m
+        medianPos = ((m+n)+1)//2
+        
+        while low <= high:
+            cut1 = (low+high)>>1
+            cut2 = medianPos - cut1
             
-            if nums[mid] <= med:
-                start = mid+1
+            l1 = -(10**7) if cut1==0 else nums1[cut1-1]
+            l2 = -(10**7) if cut2==0 else nums2[cut2-1]
+            r1 =  (10**7) if cut1==m else nums1[cut1]
+            r2 =  (10**7) if cut2==n else nums2[cut2]
             
-            else:
-                end = mid-1
-        
-        return start 
-        
-    '''Returns the number which has n integers less than or equal to it'''
-    def median_calc(self,nums1,nums2,n):
-        
-        Min = min(nums1[0],nums2[0])
-        Max = max(nums1[-1],nums2[-1])
-        
-        while Min<=Max:
-            mid = (Min+Max)//2
             
-            n1 = self.ncalc(nums1,mid)+self.ncalc(nums2,mid)
+            if l1<=r2 and l2<=r1:
+                if (m+n)%2:
+                    return max(l1,l2)
+                else:
+                    return (max(l1,l2)+min(r1,r2))/2
             
-            if n1<=n:
-                Min = mid+1
+            elif l1>r2:
+                high = cut1-1
             
             else:
-                Max = mid-1
-            
-        return Min
+                low = cut1+1
         
-    '''Returns the median of 1D sorted array'''
-    def median_calc1(self,nums):
-        
-        n = len(nums)
-        if n%2:
-            return nums[n//2]
-        
-        else:
-            return (nums[n//2] + nums[(n//2)-1])/2
     
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
-        
-        if len(nums2) == 0:
-            return self.median_calc1(nums1)
-        
-        elif len(nums1) == 0:
-            return self.median_calc1(nums2)
-        
-        
-        n = len(nums1)+len(nums2)
-        
-        if n%2==0:
-            median1 = self.median_calc(nums1,nums2,(n//2))
-            median2 = self.median_calc(nums1,nums2,(n//2)-1)
-            
-            return (median1+median2)/2
-        
-        else:
-            median = self.median_calc(nums1,nums2,n//2)
-            return median
+        return self.median(nums1,nums2,len(nums1),len(nums2))

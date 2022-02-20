fd747d140b0f3425719826bf660be53156f62345
Sindhu <70776993+Sinplified@users.noreply.github.com>
2022-02-20 07:37:50 +0530

    Time: 44 ms (43.54%), Space: 14.1 MB (70.95%) - LeetHub

--- 232-implement-queue-using-stacks/232-implement-queue-using-stacks.py
+++ 232-implement-queue-using-stacks/232-implement-queue-using-stacks.py
@@ -3,46 +3,30 @@ from queue import LifoQueue
 class MyQueue:
 
     def __init__(self):
-        self.stack1 = LifoQueue(maxsize = 100)
-        self.stack2 = LifoQueue(maxsize = 100)
-        self.curr_con = True
-    
-    def can_pop(self):
-        return  not self.curr_con
-    
-    def can_push(self):
-        return self.curr_con
-    
-    def reverse(self):
-        s1 = self.stack1
-        s2 = self.stack2
+        self.input = LifoQueue(maxsize = 100)
+        self.output = LifoQueue(maxsize = 100)
         
-        while not s1.empty():
-            s2.put(s1.get())
-        
-        self.stack1,self.stack2 = s2,s1
-        self.curr_con = not self.curr_con
 
     def push(self, x: int) -> None:
-        if not self.can_push():
-            self.reverse()
-        
-        self.stack1.put(x)
+        self.input.put(x)
 
     def pop(self) -> int:
-        if not self.can_pop():
-            self.reverse()
+        if self.output.empty():
+            while not self.input.empty():
+                self.output.put(self.input.get())
         
-        return self.stack1.get()
+        x = self.output.get()
+        return x
 
     def peek(self) -> int:
-        if not self.can_pop():
-            self.reverse()
+        if self.output.empty():
+            while not self.input.empty():
+                self.output.put(self.input.get())
         
-        return self.stack1.queue[-1]
+        return self.output.queue[-1]
 
     def empty(self) -> bool:
-        return self.stack1.empty()
+        return self.input.empty() and self.output.empty()
 
 
 # Your MyQueue object will be instantiated and called as such:

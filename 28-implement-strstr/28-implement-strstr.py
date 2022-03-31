34fda33e41f59eb1eaf33d6f27fc6b93fcd814c3
Sindhu <70776993+Sinplified@users.noreply.github.com>
2022-03-31 12:51:52 +0530

    Time: 198 ms (15.84%), Space: 16.8 MB (5.31%) - LeetHub

--- 28-implement-strstr/28-implement-strstr.py
+++ 28-implement-strstr/28-implement-strstr.py
@@ -1,46 +1,48 @@
 class Solution:
     
-    def __init__(self):
-        self.BASE = 10**6
-    
-    def strStr(self, haystack: str, needle: str) -> int:
+    def calculateZ(self,Input:str,n:int):
+        z =[0]*len(Input)
+        left = right = 0
         
-        if len(needle)==0 or needle==haystack:
-            return 0
-        
-        return self.Rabin_karp(haystack,needle)
-        
-    def Rabin_karp(self,source:str,target:str)-> int :
-        
-        m = len(target)
-        power = 1
-        
-        for i in range(m):
-            power = (power*31)%(self.BASE)
-        
-        targetCode = 0
-        for i in range(m):
-            targetCode = (targetCode*31 + ord(target[i]) - 96)%(self.BASE)
-        
-        hashcode = 0
-        
-        
-        for i in range(len(source)):
+        for k in range(len(Input)):
+            if k>right:
+                left = right = k
+                while right<len(Input) and Input[right] == Input[right-left]:
+                    right += 1
+                
+                z[k] = right - left
+                right -= 1
+            else:
+                k1 = k - left
+                
+                if z[k1] < right-k+1:
+                    z[k] = z[k1]
+                
+                else:
+                
+                    left = k
+                    while right<len(Input) and Input[right] == Input[right - left]:
+                        right +=1
+                    
+                    z[k] = right-left
+                    right -= 1
+                
+                
             
-            hashcode = (hashcode*31 + ord(source[i])-96)%(self.BASE)
-            
-            if i<m-1:
-                continue
-            
-            if i>=m:
-                hashcode = (hashcode - (ord(source[i-m])-96)*power)%(self.BASE)
-        
-            if hashcode<0 :
-                hashcode += BASE
-
-            if hashcode == targetCode:
-                if source[i-m+1: i+1] == target:
-                    return i-m+1
+            if z[k] == n:
+                return k-n-1
         
         return -1
         
+    def matchPattern(self,text:str,pattern:str) -> int :
+        
+        newString = pattern + '$' + text
+        
+        return self.calculateZ(newString,len(pattern))
+    
+    def strStr(self, haystack: str, needle: str) -> int:
+        if len(needle)==0 or needle == haystack:
+            return 0
+        
+        return self.matchPattern(haystack,needle)
+    
